cb0ae178eecd3b211a15915b91a2889c397040ed
Jack Moffitt <jack@metajack.im>
2008-12-09 13:57:43 -0700

    Remove spurious error notices on EINTR and EAGAIN in xmpp_run_once().

--- src/event.c
+++ src/event.c
@@ -222,8 +222,9 @@ void xmpp_run_once(xmpp_ctx_t *ctx, const unsigned long timeout)
 
     /* select errored */
     if (ret < 0) {
-	xmpp_error(ctx, "xmpp", "event watcher internal error %d", 
-                   sock_error());
+	if (!sock_is_recoverable(sock_error()))
+	    xmpp_error(ctx, "xmpp", "event watcher internal error %d", 
+		       sock_error());
 	return;
     }
     
