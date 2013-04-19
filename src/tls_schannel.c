840bfda3329130d1b39477b1f0ea3c8cac8f89c7
KuRi <luisan@tid.es>
2013-04-19 22:45:02 +0200

    Changed TimedOut error to Conn Reset

--- src/tls_schannel.c
+++ src/tls_schannel.c
@@ -456,7 +456,7 @@ int tls_read(tls_t *tls, void * const buff, const size_t len)
 		      tls->recvbuffermaxlen - tls->recvbufferpos);
 			  
     if (bytes == 0) {
-        tls->lasterror = WSAETIMEDOUT;
+		tls->lasterror = WSAECONNRESET;
         return -1;
     }				  
 
