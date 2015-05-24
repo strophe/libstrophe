b08766c8e46956daba010044b00c97f78b598780
Michael Santos <michael.santos@gmail.com>
2015-05-24 10:55:02 -0400

    Namespace SHA functions
    Fix statically linking against libstrophe by renaming the internal SHA
    functions:
    
    https://github.com/strophe/libstrophe/issues/40
    
    Although the same function names are used by libstrophe and OpenSSL,
    the signatures and contexts of the SHA functions differ, resulting in
    a segfault if the OpenSSL versions are substituted.

--- src/sha1.h
+++ src/sha1.h
@@ -23,9 +23,9 @@ typedef struct {
 
 #define SHA1_DIGEST_SIZE 20
 
-void SHA1_Init(SHA1_CTX* context);
-void SHA1_Update(SHA1_CTX* context, const uint8_t* data, const size_t len);
-void SHA1_Final(SHA1_CTX* context, uint8_t digest[SHA1_DIGEST_SIZE]);
+void xmpp_SHA1_Init(SHA1_CTX* context);
+void xmpp_SHA1_Update(SHA1_CTX* context, const uint8_t* data, const size_t len);
+void xmpp_SHA1_Final(SHA1_CTX* context, uint8_t digest[SHA1_DIGEST_SIZE]);
 
 #ifdef __cplusplus
 }
