982d8404721a0bcc12ced3ae8e61d97e11f8610c
Tristan Le Guern <tleguern@bouledef.eu>
2015-07-29 15:39:30 +0200

    Signed to unsigned conversion to appease GCC

--- src/sasl.c
+++ src/sasl.c
@@ -509,7 +509,7 @@ char *base64_encode(xmpp_ctx_t *ctx,
     int clen;
     char *cbuf, *c;
     uint32_t word, hextet;
-    int i;
+    unsigned i;
 
     clen = base64_encoded_len(ctx, len);
     cbuf = xmpp_alloc(ctx, clen + 1);
@@ -587,7 +587,7 @@ unsigned char *base64_decode(xmpp_ctx_t *ctx,
     int dlen;
     unsigned char *dbuf, *d;
     uint32_t word, hextet;
-    int i;
+    unsigned i;
 
     /* len must be a multiple of 4 */
     if (len & 0x03) return NULL;
