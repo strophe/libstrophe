38aa05624d47182034d201d51ed39cbc618585c7
Dmitry Podgorny <pasis.ua@gmail.com>
2016-09-14 18:12:47 +0300

    Add warning about disabled TLS in build-android.sh
    OpenSSL or BoringSSL will be added to build-android.sh in the future.
    But for now users need to build libstrophe with openssl manually if
    they use ndk-build system.

--- build-android.sh
+++ build-android.sh
@@ -4,11 +4,19 @@ DIR=`dirname $0`
 EXPAT_PATH="$DIR/expat"
 EXPAT_FILE='lib/expat.h'
 
+echo
+echo Attention: libstrophe will be built without TLS support. To enable TLS
+echo you need to replace \'src/tls_dummy.c\' with \'src/tls_opnessl.c\' in
+echo \'jni/Android.mk\', add path to the openssl headers and link your program
+echo with libssl and libcrypto.
+echo
+
 if [ ! -d $EXPAT_PATH ]; then
     mkdir $EXPAT_PATH
 fi
 
 # TODO Accept expat tarball as argument and extract it to the right place.
+#      Or download sources from sf.net.
 
 if [ ! -d $EXPAT_PATH/lib -o ! -f "$EXPAT_PATH/$EXPAT_FILE" ]; then
     cat <<EOT
