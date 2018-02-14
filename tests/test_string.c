5b204b093851f6be0a16d4b96ed7b8c995b92568
Scott M Anderson <smanders@sdl.usu.edu>
2018-02-14 10:58:08 -0700

    fix strtok_r test on MSW

--- tests/test_string.c
+++ tests/test_string.c
@@ -20,6 +20,16 @@
 
 #include "test.h" /* ARRAY_SIZE */
 
+/* strtok_s() has appeared in visual studio 2005.
+   Use own implementation for older versions. */
+#ifdef _MSC_VER
+# if (_MSC_VER >= 1400)
+# define strtok_r strtok_s
+# else
+# define strtok_r xmpp_strtok_r
+# endif
+#endif /* _MSC_VER */
+
 static int test_strtok_r(void)
 {
     const char *test = "-abc-=-def--";
