fcc4f8c680d27526704e5849cc166486416f3f41
Steffen Jaeckel <jaeckel-floss@eyet-services.de>
2021-07-07 18:20:46 +0200

    skip further processing on TLS error
    While reading through the code this seemed logical to do.
    I haven't seen this occuring in the wild!
    
    Signed-off-by: Steffen Jaeckel <jaeckel-floss@eyet-services.de>

--- src/event.c
+++ src/event.c
@@ -110,6 +110,7 @@ void xmpp_run_once(xmpp_ctx_t *ctx, unsigned long timeout)
                 xmpp_debug(ctx, "xmpp", "Send error occurred, disconnecting.");
                 conn->error = ECONNABORTED;
                 conn_disconnect(conn);
+                goto NEXT_ITEM;
             }
         }
 
@@ -154,7 +155,7 @@ void xmpp_run_once(xmpp_ctx_t *ctx, unsigned long timeout)
             conn->error = ECONNABORTED;
             conn_disconnect(conn);
         }
-
+NEXT_ITEM:
         connitem = connitem->next;
     }
 
